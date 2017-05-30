94643327a2df42ca0082c404f61a6f1b04b6ec6c
Jim Barnes <jimbarnesdeveloper@gmail.com>
2017-05-30 13:56:01 -0400

    Initial commit

---
+++ page-assets.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: UCF Page Assets
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+Description: Enqueue a page specific stylesheet and javascript file.
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'UCF_PAGE_ASSETS__FILE', __FILE__ );
+
